1ff433f9683fd8d625f9eb95e07d88ce29ebd107
Josh Miles <jmiles77@ford.com>
2018-06-27 16:12:36 -0400

    add e2e tests for root page
    Signed-off-by: Matt Schultz <mschu124@ford.com>

--- ui/e2e/src/app.e2e-spec.ts
+++ ui/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,8 +8,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should navigate to create', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to retroquest-ui!');
+    expect(browser.getCurrentUrl()).toContain('create');
   });
 });
--- ui/e2e/src/app.po.ts
+++ ui/e2e/src/app.po.ts
@@ -4,8 +4,4 @@ export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
-
-  getParagraphText() {
-    return element(by.css('rq-root h1')).getText();
-  }
 }
