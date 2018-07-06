9d69d95ce977c5482ce81c89afa755f92ef44197
Lee Lazarecky <Druage@gmx.com>
2018-07-06 15:49:22 -0400

    Added count-separator component to columns (#35)
    - Removes the column count circle that was in the column header and puts
    in below the thought / action item input section.
    
    [#33]

---
+++ ui/src/app/modules/teams/components/count-seperator/count-seperator.component.spec.ts
@@ -0,0 +1,13 @@
+import {CountSeperatorComponent} from './count-seperator.component';
+
+describe('CountSeperatorComponent', () => {
+  let component: CountSeperatorComponent;
+
+  beforeEach(() => {
+    component = new CountSeperatorComponent();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
