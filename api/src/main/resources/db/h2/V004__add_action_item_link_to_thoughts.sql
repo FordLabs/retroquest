4b128a50499b6c09ed5e402f8b261a0dccab24be
Lee Lazarecky <llazarecky@ford.com>
2019-02-22 10:18:30 -0500

    Add Action to Thought relational table
    Signed-off-by: Patrick Saperstein <patricksaperstein@gmail.com>

---
+++ api/src/main/resources/db/h2/V004__add_action_item_link_to_thoughts.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `action_thought_map` (
+  `thought_id` bigint(20) NOT NULL,
+  `action_item_id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+);
