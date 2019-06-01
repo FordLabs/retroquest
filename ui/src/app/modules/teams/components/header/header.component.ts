17545a497c2be6659da48db7b628276d97cb40c1
Lee Lazarecky <leelazarecky@gmail.com>
2019-05-31 22:26:14 -0400

    Remove end retro button from techtrek board.

--- ui/src/app/modules/teams/components/header/header.component.ts
+++ ui/src/app/modules/teams/components/header/header.component.ts
@@ -56,6 +56,10 @@ export class HeaderComponent implements OnInit {
   public ngOnInit(): void {
   }
 
+  get boardIsOpenToEveryone(): boolean {
+    return this.teamName.toLowerCase() === 'techtrek';
+  }
+
   get darkThemeIsEnabled(): boolean {
     return this.theme === Themes.Dark;
   }
