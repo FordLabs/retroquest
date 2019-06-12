89523dec4eef72211e1796c851521c1b3f5fe8d7
Lee Lazarecky <leelazarecky@gmail.com>
2019-06-12 00:13:11 -0400

    Renamed status code exceptions folder to keep it consistent.

--- api/src/main/java/com/ford/labs/retroquest/statuscodeexceptions/BadRequestException.java
+++ api/src/main/java/com/ford/labs/retroquest/statuscodeexceptions/BadRequestException.java
@@ -1,4 +1,4 @@
-package com.ford.labs.retroquest.StatusCodeExceptions;
+package com.ford.labs.retroquest.statuscodeexceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
