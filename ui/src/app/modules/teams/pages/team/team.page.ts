6c1db4659085b6769c836d77caa84cf4fdb5448d
Mike Smithson <msmithso@ford.com>
2020-08-21 09:48:50 -0400

    refactoring some collection processing constructs

--- ui/src/app/modules/teams/pages/team/team.page.ts
+++ ui/src/app/modules/teams/pages/team/team.page.ts
@@ -144,15 +144,15 @@ export class TeamPageComponent implements OnInit {
     const thoughts = [];
     const archivedActionItems: Array<ActionItem> = [];
 
-    this.columnsAggregation.map((column) => {
+    this.columnsAggregation.forEach((column) => {
       if (column.topic !== 'action') {
         thoughts.push(...column.items.active);
         thoughts.push(...column.items.completed);
       } else {
         archivedActionItems.push(...column.items.completed as Array<ActionItem>);
-        for (let i = 0; i < archivedActionItems.length; ++i) {
-          archivedActionItems[i].archived = true;
-        }
+        archivedActionItems.forEach((actionItem: ActionItem) => {
+          actionItem.archived = true;
+        });
       }
     });
 
@@ -160,15 +160,11 @@ export class TeamPageComponent implements OnInit {
       if (thoughts.length > 0) {
         this.boardService.createBoard(this.teamId, thoughts).subscribe();
       }
-
       if (archivedActionItems.length > 0) {
         this.actionItemService.archiveActionItems(archivedActionItems);
       }
-
       this.websocketService.endRetro();
     }
-
-
   }
 
   public isSelectedIndex(index: number): boolean {
