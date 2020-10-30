748897515ac2e20dc8788f30813e2f14b5d5ec5c
Mike Smithson <msmithso@ford.com>
2020-10-30 16:51:56 -0400

    adding swagger to MetricsController
    Co-authored-by: Mike Smithson <msmithso@ford.com>

--- api/src/main/java/com/ford/labs/retroquest/metrics/MetricsController.java
+++ api/src/main/java/com/ford/labs/retroquest/metrics/MetricsController.java
@@ -1,5 +1,9 @@
 package com.ford.labs.retroquest.metrics;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +15,7 @@ import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/admin/metrics")
+@Api(tags = {"Metrics Controller"}, description = "The controller that manages metrics")
 public class MetricsController {
 
     private final Metrics metrics;
@@ -20,6 +25,8 @@ public class MetricsController {
     }
 
     @GetMapping("/team/count")
+    @ApiOperation(value = "Gets the number of teams between a start and end date", notes = "getTeamCount")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Long.class)})
     public ResponseEntity<Long> getTeamCount(
             @RequestParam(name = "start", required = false)
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
@@ -32,6 +39,8 @@ public class MetricsController {
     }
 
     @GetMapping("/feedback/count")
+    @ApiOperation(value = "Gets the number of feedback entries between a start and end date", notes = "saveFeedback")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Long.class)})
     public ResponseEntity<Integer> getFeedbackCount(
             @RequestParam(name = "start", required = false)
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
@@ -44,6 +53,8 @@ public class MetricsController {
     }
 
     @GetMapping("/feedback/average")
+    @ApiOperation(value = "Returns the average app rating between a start and end date", notes = "saveFeedback")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Double.class)})
     public ResponseEntity<Double> getAverageRating(
             @RequestParam(name = "start", required = false)
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
@@ -56,6 +67,8 @@ public class MetricsController {
     }
 
     @GetMapping("/team/logins")
+    @ApiOperation(value = "Returns the number of logins given a start and end date", notes = "saveFeedback")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Integer.class)})
     public int getLogins(
             @RequestParam(name = "start", required = false)
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
