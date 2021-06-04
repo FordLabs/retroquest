25ab9c915fa8b41be7cea1b7a4f1613431830df4
Tanmay Satam <tsatam@ford.com>
2021-06-04 17:07:34 -0400

    Add test for feedback endpoint

--- api/src/test/java/com/ford/labs/retroquest/api/FeedbackApiTest.java
+++ api/src/test/java/com/ford/labs/retroquest/api/FeedbackApiTest.java
@@ -9,10 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,28 +33,47 @@ public class FeedbackApiTest extends ApiTest {
     }
 
     @Test
+    public void shouldPostFeedbackAndUpdateMetric() throws Exception {
+        var feedback = Feedback.builder()
+            .stars(4)
+            .comment("This is a comment")
+            .userEmail("email@email.email")
+            .teamId("teamId")
+            .dateCreated(null)
+            .build();
+
+        mockMvc.perform(
+            post("/api/feedback/")
+                .content(objectMapper.writeValueAsString(feedback))
+                .contentType(APPLICATION_JSON)
+        )
+            .andExpect(status().isCreated())
+            .andReturn();
+    }
+
+    @Test
     public void should_get_all_feedback_as_an_admin() throws Exception {
         feedbackRepository.save(Feedback.builder().build());
 
         mockMvc.perform(
-                get("/api/admin/feedback/all")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .with(httpBasic(getAdminUsername(), getAdminPassword())))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)));
+            get("/api/admin/feedback/all")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(httpBasic(getAdminUsername(), getAdminPassword())))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(1)));
     }
 
     @Test
     public void should_not_get_all_feedback_being_unauthorized() throws Exception {
         mockMvc.perform(get("/api/admin/feedback/all").contentType(MediaType.APPLICATION_JSON)
-                .with(httpBasic("foo", "bar")))
-                .andExpect(status().isUnauthorized());
+            .with(httpBasic("foo", "bar")))
+            .andExpect(status().isUnauthorized());
     }
 
     @Test
     public void should_not_get_all_feedback_without_basic_auth_token() throws Exception {
         mockMvc.perform(get("/api/admin/feedback/all")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isUnauthorized());
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isUnauthorized());
     }
 }
