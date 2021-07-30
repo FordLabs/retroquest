aece2fdd8d76da68809ddef172de467d6f623d5f
Matt Schultz <mschu124@ford.com>
2021-07-30 12:06:18 -0400

    Remove theme prop from FloatingCharacterCountdown
    Co-authored-by: Matt Schultz <mschu124@ford.com>
    Co-authored-by: Nick Reuter <thenewimagineer@gmail.com>

--- ui/src/react/components/floating-character-countdown/FloatingCharacterCountdown.tsx
+++ ui/src/react/components/floating-character-countdown/FloatingCharacterCountdown.tsx
@@ -1,29 +1,26 @@
 import * as React from 'react';
-import { Themes } from '../../../app/modules/domain/Theme';
+import classNames from 'classnames';
 
 import './FloatingCharacterCountdown.scss';
-import classNames from 'classnames';
 
 interface Props {
   characterCount: number;
   maxCharacterCount?: number;
   charsAreRunningOutThreshold?: number;
-  theme?: Themes;
 }
 
 export default function FloatingCharacterCountdown(props: Props): React.ReactElement {
-  const { maxCharacterCount = 255, charsAreRunningOutThreshold = 20, characterCount = 0, theme = Themes.Light } = props;
+  const { maxCharacterCount = 255, charsAreRunningOutThreshold = 20, characterCount = 0 } = props;
 
   const charactersRemaining = maxCharacterCount - characterCount;
   const charactersRemainingHaveRunOut = charactersRemaining <= 0;
   const charactersRemainingAreBelowThreshold = charactersRemaining < charsAreRunningOutThreshold;
   const charactersRemainingAreAboutToRunOut = charactersRemainingAreBelowThreshold && !charactersRemainingHaveRunOut;
 
-  const className = classNames({
+  const className = classNames('floating-character-countdown', {
     visible: characterCount,
     'display-warning-text': charactersRemainingAreAboutToRunOut,
     'display-error-text': charactersRemainingHaveRunOut,
-    'dark-theme': theme === Themes.Dark,
   });
 
   return <span className={className}>{charactersRemaining}</span>;
