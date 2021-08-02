d111c357ba772cee715ba4ecc020e2d83633d439
Matt Schultz <mschu124@ford.com>
2021-08-02 17:55:40 -0400

    Add Tooltip react component, tests, and story

--- ui/src/react/components/floating-character-countdown/FloatingCharacterCountdown.spec.tsx
+++ ui/src/react/components/floating-character-countdown/FloatingCharacterCountdown.spec.tsx
@@ -15,93 +15,48 @@
  * limitations under the License.
  */
 import { render, screen } from '@testing-library/react';
+
 import FloatingCharacterCountdown from './FloatingCharacterCountdown';
 
-describe('FloatingCharacterCountdownComponent', () => {
+describe('FloatingCharacterCountdown', () => {
   describe('visibility', () => {
     it('should be hidden when character count is 0', () => {
-      render(
-        <FloatingCharacterCountdown
-          characterCount={0}
-          maxCharacterCount={100}
-        />
-      );
+      render(<FloatingCharacterCountdown characterCount={0} maxCharacterCount={100} />);
       expect(screen.getByText('100').className).not.toContain('visible');
     });
 
     it('should show the number of remaining characters', () => {
-      render(
-        <FloatingCharacterCountdown
-          characterCount={1}
-          maxCharacterCount={100}
-        />
-      );
+      render(<FloatingCharacterCountdown characterCount={1} maxCharacterCount={100} />);
       expect(screen.getByText('99').className).toContain('visible');
     });
   });
 
   describe('characters are about to run out', () => {
     it('should warn if the remaining characters to input are less than threshold', () => {
-      render(
-        <FloatingCharacterCountdown
-          characterCount={2}
-          maxCharacterCount={3}
-          charsAreRunningOutThreshold={2}
-        />
-      );
+      render(<FloatingCharacterCountdown characterCount={2} maxCharacterCount={3} charsAreRunningOutThreshold={2} />);
       expect(screen.getByText('1').className).toContain('display-warning-text');
     });
 
     it('should not warn if the remaining characters to input are greater than or equal to the threshold', () => {
-      render(
-        <FloatingCharacterCountdown
-          characterCount={1}
-          maxCharacterCount={3}
-          charsAreRunningOutThreshold={2}
-        />
-      );
-      expect(screen.getByText('2').className).not.toContain(
-        'display-warning-text'
-      );
+      render(<FloatingCharacterCountdown characterCount={1} maxCharacterCount={3} charsAreRunningOutThreshold={2} />);
+      expect(screen.getByText('2').className).not.toContain('display-warning-text');
     });
 
     it('should not warn if the remaining characters have run out', () => {
-      render(
-        <FloatingCharacterCountdown
-          characterCount={3}
-          maxCharacterCount={3}
-          charsAreRunningOutThreshold={2}
-        />
-      );
-      expect(screen.getByText('0').className).not.toContain(
-        'display-warning-text'
-      );
+      render(<FloatingCharacterCountdown characterCount={3} maxCharacterCount={3} charsAreRunningOutThreshold={2} />);
+      expect(screen.getByText('0').className).not.toContain('display-warning-text');
     });
   });
 
   describe('characters have run out', () => {
     it('should error if the remaining characters are less than or equal to 0', () => {
-      render(
-        <FloatingCharacterCountdown
-          characterCount={3}
-          maxCharacterCount={3}
-          charsAreRunningOutThreshold={2}
-        />
-      );
+      render(<FloatingCharacterCountdown characterCount={3} maxCharacterCount={3} charsAreRunningOutThreshold={2} />);
       expect(screen.getByText('0').className).toContain('display-error-text');
     });
 
     it('should not error if the remaining characters are greater than 0', () => {
-      render(
-        <FloatingCharacterCountdown
-          characterCount={2}
-          maxCharacterCount={3}
-          charsAreRunningOutThreshold={2}
-        />
-      );
-      expect(screen.getByText('1').className).not.toContain(
-        'display-error-text'
-      );
+      render(<FloatingCharacterCountdown characterCount={2} maxCharacterCount={3} charsAreRunningOutThreshold={2} />);
+      expect(screen.getByText('1').className).not.toContain('display-error-text');
     });
   });
 });
