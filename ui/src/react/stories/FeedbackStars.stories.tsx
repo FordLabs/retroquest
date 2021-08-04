2d33a0c31ac4583fd55773ccff075ca1c3d3da72
Matt Schultz <mschu124@ford.com>
2021-08-04 12:23:21 -0400

    Add FeedbackStars react component, tests, and story

---
+++ ui/src/react/stories/FeedbackStars.stories.tsx
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import FeedbackStars from '../components/feedback-stars/FeedbackStars';
+
+export default {
+  title: 'components/FeedbackStars',
+  component: FeedbackStars,
+} as ComponentMeta<typeof FeedbackStars>;
+
+const Template: ComponentStory<typeof FeedbackStars> = () => {
+  const [stars, setStars] = React.useState(0);
+
+  return (
+    <FeedbackStars
+      value={stars}
+      onChange={(newStars) => {
+        setStars(newStars);
+      }}
+    />
+  );
+};
+
+export const Example = Template.bind({});
