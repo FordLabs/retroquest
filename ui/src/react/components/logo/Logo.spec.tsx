81ec3b16aa83519b6f4df372607252b3eaab26bc
Matt Schultz <mschu124@ford.com>
2021-08-04 13:53:12 -0400

    Add Ford Copyright header to files missing it

--- ui/src/react/components/logo/Logo.spec.tsx
+++ ui/src/react/components/logo/Logo.spec.tsx
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 
