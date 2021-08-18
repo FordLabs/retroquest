71b5026a97ac16fefa342b31fe12f5ff551d4ada
Matt Schultz <mschu124@ford.com>
2021-08-18 11:47:20 -0400

    Make Modal controllable through props

--- ui/src/react/components/modal/Modal.spec.tsx
+++ ui/src/react/components/modal/Modal.spec.tsx
@@ -25,38 +25,116 @@ const ref = React.createRef<ModalMethods>();
 
 describe('Modal', () => {
   beforeEach(() => {
-    render(
-      <Modal ref={ref}>
-        <div>Modal Content</div>
-      </Modal>
-    );
+    jest.clearAllMocks();
+  });
 
-    act(() => {
-      ref.current.show();
+  describe('uncontrolled', () => {
+    beforeEach(() => {
+      render(
+        <Modal ref={ref}>
+          <div>Modal Content</div>
+        </Modal>
+      );
+
+      act(() => {
+        ref.current.show();
+      });
+    });
+
+    it('should show and hide by ref methods', () => {
+      screen.getByText('Modal Content');
+
+      act(() => {
+        ref.current.hide();
+      });
+
+      expect(screen.queryByText('Modal Content')).toBeFalsy();
+    });
+
+    it('should hide only when backdrop is clicked', () => {
+      userEvent.click(screen.getByText('Modal Content'));
+      screen.getByText('Modal Content');
+
+      userEvent.click(screen.getByTestId('modalBackdrop'));
+      expect(screen.queryByText('Modal Content')).toBeFalsy();
+    });
+
+    it('should hide when escape key is pressed', () => {
+      userEvent.type(document.body, '{escape}');
+
+      expect(screen.queryByText('Modal Content')).toBeFalsy();
     });
   });
 
-  it('should show and hide by ref methods', () => {
-    screen.getByText('Modal Content');
+  describe('controlled', () => {
+    const mockClose = jest.fn();
 
-    act(() => {
-      ref.current.hide();
+    it('should show and hide by prop', () => {
+      const { rerender } = render(
+        <Modal ref={ref} show={true} onHide={mockClose}>
+          <div>Modal Content</div>
+        </Modal>
+      );
+
+      screen.getByText('Modal Content');
+
+      rerender(
+        <Modal ref={ref} show={false}>
+          <div>Modal Content</div>
+        </Modal>
+      );
+
+      expect(screen.queryByText('Modal Content')).toBeFalsy();
     });
 
-    expect(screen.queryByText('Modal Content')).toBeFalsy();
-  });
+    it('should call onHide when backdrop is clicked', () => {
+      render(
+        <Modal ref={ref} show={true} onHide={mockClose}>
+          <div>Modal Content</div>
+        </Modal>
+      );
 
-  it('should hide only when backdrop is clicked', () => {
-    userEvent.click(screen.getByText('Modal Content'));
-    screen.getByText('Modal Content');
+      userEvent.click(screen.getByText('Modal Content'));
+      expect(mockClose).not.toHaveBeenCalled();
 
-    userEvent.click(screen.getByTestId('modalBackdrop'));
-    expect(screen.queryByText('Modal Content')).toBeFalsy();
-  });
+      userEvent.click(screen.getByTestId('modalBackdrop'));
+      expect(mockClose).toHaveBeenCalledTimes(1);
+    });
 
-  it('should hide when escape key is pressed', () => {
-    userEvent.type(document.body, '{escape}');
+    it('should call onHide when escape key is pressed', () => {
+      render(
+        <Modal ref={ref} show={true} onHide={mockClose}>
+          <div>Modal Content</div>
+        </Modal>
+      );
 
-    expect(screen.queryByText('Modal Content')).toBeFalsy();
+      userEvent.type(document.body, '{escape}');
+
+      expect(mockClose).toHaveBeenCalledTimes(1);
+    });
+
+    it('can turn off closing on backdrop click', () => {
+      render(
+        <Modal ref={ref} show={true} onHide={mockClose} hideOnBackdropClick={false}>
+          <div>Modal Content</div>
+        </Modal>
+      );
+
+      userEvent.click(screen.getByTestId('modalBackdrop'));
+
+      expect(mockClose).not.toHaveBeenCalled();
+    });
+
+    it('can turn off closing on escape key', () => {
+      render(
+        <Modal ref={ref} show={true} onHide={mockClose} hideOnEscape={false}>
+          <div>Modal Content</div>
+        </Modal>
+      );
+
+      userEvent.type(document.body, '{escape}');
+
+      expect(mockClose).not.toHaveBeenCalled();
+    });
   });
 });
--- ui/src/react/components/modal/Modal.tsx
+++ ui/src/react/components/modal/Modal.tsx
@@ -1,48 +1,84 @@
 import * as React from 'react';
 import classnames from 'classnames';
 
+import { onKeys } from '../../utils/EventUtils';
+
 import './Modal.scss';
+
+const NO_OP = () => undefined;
 
 export interface ModalMethods {
   show: () => void;
   hide: () => void;
+  setHideOnEscape: (enabled: boolean) => void;
+  setHideOnBackdropClick: (enabled: boolean) => void;
 }
 
-const ModalContext = React.createContext<ModalMethods>({ show: () => undefined, hide: () => undefined });
+const ModalContext = React.createContext<ModalMethods>({
+  show: NO_OP,
+  hide: NO_OP,
+  setHideOnEscape: NO_OP,
+  setHideOnBackdropClick: NO_OP,
+});
 
-type ModalProps = React.PropsWithChildren<{ className?: string }>;
+type ModalProps = React.PropsWithChildren<{
+  show?: boolean;
+  hideOnEscape?: boolean;
+  hideOnBackdropClick?: boolean;
+  onHide?: () => void;
+  className?: string;
+}>;
 
 function Modal(props: ModalProps, ref: React.Ref<ModalMethods>) {
-  const { className, children } = props;
+  const {
+    show: showProp,
+    hideOnEscape: hideOnEscapeProp,
+    hideOnBackdropClick: hideOnBackdropClickProp,
+    onHide,
+    className,
+    children,
+  } = props;
 
-  const [show, setShow] = React.useState(false);
+  const [showState, setShowState] = React.useState(false);
+  const [hideOnEscapeState, setHideOnEscapeState] = React.useState(true);
+  const [hideOnBackdropClickState, setHideOnBackdropClickState] = React.useState(true);
+
+  const show = showProp ?? showState;
+  const hideOnEscape = hideOnEscapeProp ?? hideOnEscapeState;
+  const hideOnBackdropClick = hideOnBackdropClickProp ?? hideOnBackdropClickState;
+
+  const hide = React.useCallback(() => {
+    if (showProp === undefined) {
+      setShowState(false);
+    }
+    if (onHide) onHide();
+  }, [showProp, onHide]);
+
+  React.useEffect(() => {
+    if (show && hideOnEscape) {
+      const escapeListener = onKeys('Escape', hide);
+      document.addEventListener('keydown', escapeListener);
+
+      return () => document.removeEventListener('keydown', escapeListener);
+    }
+  }, [show, hide, hideOnEscape]);
 
   const modalMethods = React.useMemo(
     () => ({
-      show: () => setShow(true),
-      hide: () => setShow(false),
+      show: () => setShowState(true),
+      hide: () => setShowState(false),
+      setHideOnEscape: setHideOnEscapeState,
+      setHideOnBackdropClick: setHideOnBackdropClickState,
     }),
     []
   );
 
-  React.useImperativeHandle(ref, () => modalMethods);
-
-  React.useEffect(() => {
-    if (show) {
-      document.onkeydown = (event) => {
-        if (event.key === 'Escape') {
-          setShow(false);
-        }
-      };
-    } else {
-      document.onkeydown = null;
-    }
-  }, [show]);
+  React.useImperativeHandle(ref, () => modalMethods, [modalMethods]);
 
   return (
     <ModalContext.Provider value={modalMethods}>
       {show && (
-        <div className="modal-backdrop" onClick={() => setShow(false)} data-testid="modalBackdrop">
+        <div className="modal-backdrop" onClick={hideOnBackdropClick ? hide : undefined} data-testid="modalBackdrop">
           <div className={classnames('modal', className)} onClick={(event) => event.stopPropagation()}>
             {children}
           </div>
@@ -53,3 +89,7 @@ function Modal(props: ModalProps, ref: React.Ref<ModalMethods>) {
 }
 
 export default React.forwardRef<ModalMethods, ModalProps>(Modal);
+
+export function useModal() {
+  return React.useContext(ModalContext);
+}
