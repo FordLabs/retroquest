d9beb88c74c75dee76b7355f5298cf25daba6f68
theokoles <gabrieltrahan777@hotmail.com>
2021-12-21 08:36:19 -0600

    UI/Style changes for timer

--- ui/src/app/modules/components/countdown-timer/countdown-timer.component.ts
+++ ui/src/app/modules/components/countdown-timer/countdown-timer.component.ts
@@ -1,4 +1,22 @@
+ /*
+ * Copyright (c) 2021 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { Component, Input, OnInit } from '@angular/core';
+import { Themes } from '../../domain/Theme';
 
 @Component({
   selector: 'rq-countdown-timer',
@@ -6,12 +24,17 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./countdown-timer.component.scss']
 })
 export class CountdownTimerComponent implements OnInit {
+  @Input() theme: Themes = Themes.Light;
   @Input() minutes = 5;
   @Input() seconds = 0;
   time!: number;
   interval!: any;
   border = "set";
   running = false;
+
+  get darkThemeIsEnabled(): boolean {
+    return this.theme === Themes.Dark;
+  }
 
   startTimer() {
     if(this.minutes === undefined || this.minutes < 0){
