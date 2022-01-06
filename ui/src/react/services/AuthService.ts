70785377669a7112fbac9920884416e84472a74d
Ashley Clifton <aclift19@ford.com>
2022-01-06 11:16:23 -0500

    Remove rq-root styles

--- ui/src/react/services/AuthService.ts
+++ ui/src/react/services/AuthService.ts
@@ -1,14 +1,30 @@
+/*
+ * Copyright (c) 2021 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 export class AuthService {
   static tokenKey = 'token';
   static tokenDuration = 1000 * 60 * 60 * 24 * 2;
 
-  static setToken(token): void {
+  static setToken(token: string): void {
     const expiresDate = new Date(Date.now() + AuthService.tokenDuration);
     const expires = expiresDate.toUTCString();
-    document.cookie = `${AuthService.tokenKey}=${token};expires=${expires};`;
+    document.cookie = `${AuthService.tokenKey}=${token};expires=${expires};path=/`;
   }
 
-  static getToken() {
+  static getToken(): string {
     let token = null;
     const cookie = document.cookie;
     const keyIndex = cookie.indexOf(`${AuthService.tokenKey}=`);
