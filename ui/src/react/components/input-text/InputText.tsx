0e008136e1b09936edf8f97fd7ba77dc9efb49c7
Ashley Clifton <aclift19@ford.com>
2022-01-06 15:41:08 -0500

    Ensure cypress tests pass, etc
    * Convert cypress util functions to cypress commands
    * Add typescript types file for cypress custom commands
    * Ensure editor is recognizing cypress references

--- ui/src/react/components/input-text/InputText.tsx
+++ ui/src/react/components/input-text/InputText.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Ford Motor Company
+ * Copyright (c) 2022 Ford Motor Company
  * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -49,6 +49,7 @@ export default function InputText(props: InputTextProps): JSX.Element {
       )}
       <input
         id={id}
+        data-testid={id}
         className={classnames('input-text', className)}
         value={value}
         onChange={onChange}
