8e49c3336a02c2b1d9abedce299096b34ca80ffd
Andrew Crouch <jcrouc15@ford.com>
2022-01-07 11:19:30 -0500

    adds editColumnTitleName to ColumnTitleService

--- api/src/main/java/com/ford/labs/retroquest/columntitle/ColumnTitleService.java
+++ api/src/main/java/com/ford/labs/retroquest/columntitle/ColumnTitleService.java
@@ -2,7 +2,6 @@ package com.ford.labs.retroquest.columntitle;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -10,16 +9,18 @@ public class ColumnTitleService {
 
     private ColumnTitleRepository columnTitleRepository;
 
-    public ColumnTitleService(ColumnTitleRepository columnTitleRepository){
+    public ColumnTitleService(ColumnTitleRepository columnTitleRepository) {
         this.columnTitleRepository = columnTitleRepository;
     }
 
-    public List<ColumnTitle> getColumnTitlesByTeamId(String teamId){
+    public List<ColumnTitle> getColumnTitlesByTeamId(String teamId) {
         return columnTitleRepository.findAllByTeamId(teamId);
     }
 
-    public ColumnTitle editColumnTitle(Long columnId){
-        return new ColumnTitle();
+    public ColumnTitle editColumnTitleName(Long columnId, String newColumnName) {
+        var savedColumnTitle = columnTitleRepository.findById(columnId).orElseThrow();
+        savedColumnTitle.setTitle(newColumnName);
+        return columnTitleRepository.save(savedColumnTitle);
     }
 
 }
--- api/src/test/java/com/ford/labs/retroquest/columntitle/ColumnTitleServiceTest.java
+++ api/src/test/java/com/ford/labs/retroquest/columntitle/ColumnTitleServiceTest.java
@@ -1,5 +1,6 @@
 package com.ford.labs.retroquest.columntitle;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,11 @@ class ColumnTitleServiceTest {
 
     @Autowired
     ColumnTitleService columnTitleService;
+
+    @BeforeEach
+    void beforeEach() {
+        columnTitleRepository.deleteAll();
+    }
 
     @Test
     void given_teamid_get_column_titles() {
@@ -30,6 +36,19 @@ class ColumnTitleServiceTest {
         assertThat(columnTitleService.getColumnTitlesByTeamId(teamId)).containsExactlyInAnyOrder(columnTitle, columnTitle2);
         assertThat(columnTitleService.getColumnTitlesByTeamId(teamId2)).containsExactlyInAnyOrder(columnTitle3);
 
+    }
+
+    @Test
+    void given_column_id_and_column_title_rename_column_in_db_and_return_new_column_title() {
+        long columnId = 1L;
+        var columnTitle = new ColumnTitle(columnId, "happy", "Some Title", "some team id");
+        columnTitleRepository.save(columnTitle);
+
+        String newColumnName = "Some new Title";
+        var savedColumnTitle = columnTitleService.editColumnTitleName(columnId, newColumnName);
+
+        assertThat(savedColumnTitle.getTitle()).isEqualTo(newColumnName);
+        assertThat(savedColumnTitle.getId()).isEqualTo(columnId);
     }
 
 }
