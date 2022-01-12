039bce5c5deb1a7bf6b67157af61f0b075ef8830
Nick Reuter <thenewimagineer@gmail.com>
2022-01-12 13:14:49 -0500

    Remove checking user in authorization

--- api/src/main/java/com/ford/labs/retroquest/security/ApiAuthorization.java
+++ api/src/main/java/com/ford/labs/retroquest/security/ApiAuthorization.java
@@ -15,33 +15,18 @@
  * limitations under the License.
  */
 
-package com.ford.labs.retroquest.api.authorization;
+package com.ford.labs.retroquest.security;
 
-import com.ford.labs.retroquest.users.User;
-import com.ford.labs.retroquest.users.UserRepository;
+import io.jsonwebtoken.lang.Objects;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-
-import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class ApiAuthorization {
 
-    private final UserRepository userRepository;
-
-    public ApiAuthorization(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    public ApiAuthorization() {}
 
     public boolean requestIsAuthorized(Authentication authentication, String teamId) {
-        return teamId.equals(authentication.getPrincipal()) || teamBelongsToUser(authentication, teamId);
-    }
-
-    private boolean teamBelongsToUser(Authentication authentication, String teamId) {
-        Optional<User> foundUser = userRepository.findByName(authentication.getPrincipal().toString());
-        return foundUser.map(user -> user.getTeams()
-                .stream()
-                .anyMatch(team -> Objects.requireNonNull(team.getId()).equals(teamId))).orElse(false);
+        return Objects.nullSafeEquals(authentication.getPrincipal(), teamId);
     }
 }
---
+++ api/src/test/java/com/ford/labs/retroquest/security/ApiAuthorizationTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2021 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ford.labs.retroquest.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.Authentication;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class ApiAuthorizationTest {
+
+    private final Authentication authentication = mock(Authentication.class);
+    private final ApiAuthorization apiAuthorization = new ApiAuthorization();
+
+    @Test
+    void requestIsAuthorized_WithMatchingPrincipal_ReturnsTrue() {
+        given(authentication.getPrincipal()).willReturn("teamId");
+        assertThat(apiAuthorization.requestIsAuthorized(authentication, "teamId")).isTrue();
+    }
+
+    @Test
+    void requestIsAuthorized_WithoutMatchingPrincipal_ReturnsFalse() {
+        given(authentication.getPrincipal()).willReturn("notAuthorized");
+        assertThat(apiAuthorization.requestIsAuthorized(authentication, "teamId")).isFalse();
+    }
+
+}
