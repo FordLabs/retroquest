366a2adcae656bec0f9ee95289c45538c0394ee7
Ashley Clifton <randomashes@gmail.com>
2022-01-18 13:13:16 -0500

    Add validation function for confirm password input

--- ui/src/react/utils/StringUtils.ts
+++ ui/src/react/utils/StringUtils.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 export const TEAM_NAME_REGEX = /^[A-Za-z0-9 ]+$/;
 export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
@@ -24,5 +40,11 @@ export function validatePassword(password: string): string | undefined {
     return 'Password must contain at least one upper case letter.';
   } else if (!password.match(NUMBER_REGEX)) {
     return 'Password must contain at least one number.';
+  }
+}
+
+export function validateConfirmationPassword(password: string, confirmationPassword: string) {
+  if (password !== confirmationPassword) {
+    return 'Please enter matching passwords';
   }
 }
