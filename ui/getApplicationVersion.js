5835b108d1de0dd8ce313050bd04dc029effaeda
Nick Reuter <thenewimagineer@gmail.com>
2022-01-18 10:26:11 -0500

    Set application version in GitHub Actions build

---
+++ ui/getApplicationVersion.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const util = require('util');
+const child = require('child_process');
+const exec = util.promisify(child.exec);
+
+async function createVersionsFile(filename) {
+  const commitHash = (await exec('git rev-parse --short HEAD')).stdout.toString().trim();
+
+  console.log(`version: '${commitHash}'`);
+
+  fs.writeFileSync(filename, JSON.stringify({version: commitHash}), {encoding: 'utf8'});
+}
+
+createVersionsFile('src/application-version.json');
