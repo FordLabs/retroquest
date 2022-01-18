00ee74f1d284ea6efe9cb0f574ef4f30244735ac
Ashley Clifton <randomashes@gmail.com>
2022-01-18 13:13:16 -0500

    Add validation function for confirm password input

--- ui/src/react/utils/StringUtils.spec.ts
+++ ui/src/react/utils/StringUtils.spec.ts
@@ -1,4 +1,20 @@
-import { validatePassword, validateTeamName } from './StringUtils';
+/*
+ * Copyright (c) 2022 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { validateConfirmationPassword, validatePassword, validateTeamName } from './StringUtils';
 
 describe('String Utils', () => {
   const emptyTeamNameError = 'Please enter a team name.';
@@ -16,8 +32,8 @@ describe('String Utils', () => {
     ['a', undefined],
     ['1', undefined],
     ['a1', undefined],
-  ])('validating team name "%s" should return error message "%s"', (teamName, error) => {
-    expect(validateTeamName(teamName)).toBe(error);
+  ])('validating team name "%s" should return error message "%s"', (teamName, errorMessage) => {
+    expect(validateTeamName(teamName)).toBe(errorMessage);
   });
 
   const emptyPasswordError = 'Please enter a password.';
@@ -41,7 +57,16 @@ describe('String Utils', () => {
     ['abcdefgA', numberError],
     ['abcdefgA1', undefined],
     ["abcdefgA1#!@#$%^&*()_+}{[];'\\<>? ", undefined],
-  ])('validating password %s should return error message "%s"', (password, error) => {
-    expect(validatePassword(password)).toBe(error);
+  ])('validating password %s should return error message "%s"', (password, errorMessage) => {
+    expect(validatePassword(password)).toBe(errorMessage);
   });
+
+  const misMatchingPasswords = 'Please enter matching passwords';
+
+  it.each([['CorrectPassword1', 'WrongConfirmationPassword', misMatchingPasswords]])(
+    'validating password: "%s" and confirmation password "%s" should return error message "%s"',
+    (password, confirmationPassword, errorMessage) => {
+      expect(validateConfirmationPassword(password, confirmationPassword)).toBe(errorMessage);
+    }
+  );
 });
