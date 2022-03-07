cae9d9e8d9a9036b1cbf5af525f8ac847eff2fa2
Nick Reuter <thenewimagineer@gmail.com>
2022-03-07 13:52:56 -0500

    #376 Group and sort thought on Archived Board

--- ui/src/react/pages/archives/thought-archives/ArchivedBoard.spec.tsx
+++ ui/src/react/pages/archives/thought-archives/ArchivedBoard.spec.tsx
@@ -47,6 +47,15 @@ describe('Archived Board', () => {
     const thought = screen.getByTestId('thought102');
     expect(within(thought).queryByText('Discussed')).not.toBeNull();
   });
+
+  it('should display thoughts in order by discussion status and then thought count', () => {
+    render(<ArchivedBoard board={singleColumnTestBoard} />);
+    const thoughts = screen.getAllByTestId(/thought/);
+    expect(within(thoughts[0]).queryByText('I am a message4')).not.toBeNull();
+    expect(within(thoughts[1]).queryByText('I am a message1')).not.toBeNull();
+    expect(within(thoughts[2]).queryByText('I am a message2')).not.toBeNull();
+    expect(within(thoughts[3]).queryByText('I am a message3')).not.toBeNull();
+  });
 });
 
 const testBoard: Board = {
@@ -93,6 +102,49 @@ const testBoard: Board = {
       id: 11,
       title: 'Sad',
       topic: Topic.UNHAPPY,
+    },
+  ],
+};
+
+const singleColumnTestBoard: Board = {
+  dateCreated: new Date(),
+  id: 1,
+  teamId: '',
+  thoughts: [
+    {
+      id: 100,
+      message: 'I am a message1',
+      hearts: 10,
+      discussed: false,
+      topic: Topic.HAPPY,
+    },
+    {
+      id: 101,
+      message: 'I am a message2',
+      hearts: 30,
+      discussed: true,
+      topic: Topic.HAPPY,
+    },
+    {
+      id: 102,
+      message: 'I am a message3',
+      hearts: 10,
+      discussed: true,
+      topic: Topic.HAPPY,
+    },
+    {
+      id: 103,
+      message: 'I am a message4',
+      hearts: 20,
+      discussed: false,
+      topic: Topic.HAPPY,
+    },
+  ],
+  columns: [
+    {
+      id: 10,
+      title: 'Happy',
+      topic: Topic.HAPPY,
     },
   ],
 };
