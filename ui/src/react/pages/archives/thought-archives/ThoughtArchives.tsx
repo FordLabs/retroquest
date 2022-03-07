03fe7d92bb29a775319f02055df67914cd61eba2
Ashley Clifton <randomashes@gmail.com>
2022-03-07 16:52:30 -0500

    Clear selected archived board on destroy for ThoughtArchives.tsx

--- ui/src/react/pages/archives/thought-archives/ThoughtArchives.tsx
+++ ui/src/react/pages/archives/thought-archives/ThoughtArchives.tsx
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 
 import { ArchivedBoardState } from '../../../state/ArchivedBoardState';
@@ -25,6 +25,10 @@ import ArchivedBoardsList from './archived-boards-list/ArchivedBoardsList';
 
 function ThoughtArchives(): JSX.Element {
   const [selectedArchivedBoard, setSelectedArchivedBoard] = useRecoilState(ArchivedBoardState);
+
+  useEffect(() => {
+    return () => setSelectedArchivedBoard(null);
+  }, []);
 
   return selectedArchivedBoard ? (
     <ArchivedBoard board={selectedArchivedBoard} />
