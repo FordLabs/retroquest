4f7ff0dd4ce991dc400dae8f952e5e09a5fcc5ef
Nick Reuter <thenewimagineer@gmail.com>
2022-03-07 13:52:56 -0500

    #376 Group and sort thought on Archived Board

--- ui/src/react/pages/archives/thought-archives/ArchivedBoard.tsx
+++ ui/src/react/pages/archives/thought-archives/ArchivedBoard.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
 import Board from '../../../types/Board';
+import { Column } from '../../../types/Column';
+import Thought from '../../../types/Thought';
 
 import ArchivedBoardColumn from './ArchivedBoardColumn';
 
@@ -11,16 +13,21 @@ interface Props {
 }
 
 function ArchivedBoard({ board }: Props): JSX.Element {
+  function getThoughts(board: Board, column: Column): Thought[] {
+    const columnSpecificThoughts = board.thoughts.filter((thought) => thought.topic === column.topic);
+    const discussedThoughts = columnSpecificThoughts
+      .filter((thought) => thought.discussed)
+      .sort((a, b) => b.hearts - a.hearts);
+    const notDiscussedThoughts = columnSpecificThoughts
+      .filter((thought) => !thought.discussed)
+      .sort((a, b) => b.hearts - a.hearts);
+    return notDiscussedThoughts.concat(discussedThoughts);
+  }
+
   return (
     <div className="archived-board">
       {board.columns.map((column) => {
-        return (
-          <ArchivedBoardColumn
-            key={column.id}
-            column={column}
-            thoughts={board.thoughts.filter((thought) => thought.topic === column.topic)}
-          />
-        );
+        return <ArchivedBoardColumn key={column.id} column={column} thoughts={getThoughts(board, column)} />;
       })}
     </div>
   );
