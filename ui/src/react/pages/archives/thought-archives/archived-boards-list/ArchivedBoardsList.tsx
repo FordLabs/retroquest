0cfd647da83aa1f202bad1922bd75847d87fe285
Nick Reuter <thenewimagineer@gmail.com>
2022-03-07 16:31:12 -0500

    #376 Add missing copyright notice to UI files

--- ui/src/react/pages/archives/thought-archives/archived-boards-list/ArchivedBoardsList.tsx
+++ ui/src/react/pages/archives/thought-archives/archived-boards-list/ArchivedBoardsList.tsx
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import React, { useCallback, useEffect, useState } from 'react';
 import classnames from 'classnames';
 import moment from 'moment';
