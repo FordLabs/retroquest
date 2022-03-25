ace7b563c86c86d5501f2e377aa7cf5289244f84
Nick Reuter <thenewimagineer@gmail.com>
2022-03-25 14:39:27 -0400

    Remove unused Board endpoints

--- api/src/main/java/com/ford/labs/retroquest/board/BoardRepository.java
+++ api/src/main/java/com/ford/labs/retroquest/board/BoardRepository.java
@@ -26,10 +26,5 @@ import java.util.List;
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findAllByTeamIdOrderByDateCreatedDesc(String teamId, Pageable pageable);
-
     List<Board> findAllByTeamId(String teamId);
-
-    void deleteBoardByTeamIdAndId(String teamId, Long id);
-
-    Board findByTeamIdAndId(String teamId, Long id);
 }
