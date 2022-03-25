e3b8a21ddebdd686513c42fb7239f8b99ee02813
Nick Reuter <thenewimagineer@gmail.com>
2022-03-25 14:19:44 -0400

    Remove unused ThoughtException constructor

--- api/src/main/java/com/ford/labs/retroquest/exception/ThoughtNotFoundException.java
+++ api/src/main/java/com/ford/labs/retroquest/exception/ThoughtNotFoundException.java
@@ -26,8 +26,4 @@ public class ThoughtNotFoundException extends RuntimeException {
     public ThoughtNotFoundException(Long thoughtId) {
         this.message = String.format("Thought with ID [%d] could not be found", thoughtId);
     }
-
-    public ThoughtNotFoundException(String thoughtId) {
-        this(Long.valueOf(thoughtId));
-    }
 }
