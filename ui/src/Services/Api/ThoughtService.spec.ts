a1055a34aec3b56b8a3e6916f669d788547d75cf
Ashley Clifton <randomashes@gmail.com>
2022-03-31 16:44:23 -0400

    Add unit tests for all functions in ThoughtService.ts

--- ui/src/Services/Api/ThoughtService.spec.ts
+++ ui/src/Services/Api/ThoughtService.spec.ts
@@ -1,92 +1,137 @@
 /*
- * Copyright (c) 2022. Ford Motor Company
- *  All rights reserved.
+ * Copyright (c) 2022 Ford Motor Company
+ * All rights reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import axios from 'axios';
 
+import { mockGetCookie } from '../../__mocks__/universal-cookie';
+import CreateThoughtRequest from '../../Types/CreateThoughtRequest';
 import Topic from '../../Types/Topic';
 
 import { getMockThought } from './__mocks__/ThoughtService';
 import ThoughtService from './ThoughtService';
 
-jest.mock('axios');
-
 describe('Thought Service', () => {
 	const teamId = 'team-id';
-	const mockConfig = { headers: { Authorization: `Bearer mock-jwt-token` } };
+	const thoughtId = 1;
+	const fakeToken = 'fake-token';
+	const mockConfig = { headers: { Authorization: `Bearer ${fakeToken}` } };
 
-	it('should get all thoughts for a team', async () => {
-		const expected = [getMockThought(Topic.HAPPY, false, 1)];
-		axios.get = jest.fn().mockResolvedValue({ data: expected });
-
-		const actual = await ThoughtService.getThoughts(teamId);
-
-		expect(actual).toEqual(expected);
-		expect(axios.get).toHaveBeenCalledWith(
-			`/api/team/${teamId}/thoughts`,
-			mockConfig
-		);
+	beforeEach(() => {
+		mockGetCookie.mockReturnValue(fakeToken);
 	});
 
-	it('should delete a thought', async () => {
-		await ThoughtService.delete(teamId, 100);
-		expect(axios.delete).toHaveBeenCalledWith(
-			`/api/team/${teamId}/thought/${100}`,
-			mockConfig
-		);
+	describe('create', () => {
+		it('should create a new thought for column', async () => {
+			const expectedThought = getMockThought(Topic.HAPPY);
+			axios.post = jest.fn().mockResolvedValue({ data: expectedThought });
+
+			const createThoughtRequest: CreateThoughtRequest = {
+				id: 1,
+				message: 'I had a thought..',
+				hearts: 5,
+				topic: Topic.HAPPY,
+				discussed: false,
+				teamId,
+			};
+
+			const actualThought = await ThoughtService.create(
+				teamId,
+				createThoughtRequest
+			);
+			expect(actualThought).toEqual(expectedThought);
+			expect(axios.post).toHaveBeenCalledWith(
+				`/api/team/${teamId}/thought`,
+				createThoughtRequest,
+				mockConfig
+			);
+		});
 	});
 
-	it('should upvote a thought', async () => {
-		await ThoughtService.upvoteThought(teamId, 100);
-		expect(axios.put).toHaveBeenCalledWith(
-			`/api/team/${teamId}/thought/${100}/heart`,
-			mockConfig
-		);
+	describe('getThoughts', () => {
+		it('should get all thoughts for current retro', async () => {
+			const expectedThoughts = [
+				getMockThought(Topic.HAPPY),
+				getMockThought(Topic.UNHAPPY),
+				getMockThought(Topic.CONFUSED),
+			];
+			axios.get = jest.fn().mockResolvedValue({ data: expectedThoughts });
+			const actualThoughts = await ThoughtService.getThoughts(teamId);
+			expect(actualThoughts).toEqual(expectedThoughts);
+			expect(axios.get).toHaveBeenCalledWith(
+				`/api/team/${teamId}/thoughts`,
+				mockConfig
+			);
+		});
 	});
 
-	it('should update discussion status of a thought', async () => {
-		await ThoughtService.updateDiscussionStatus(teamId, 100, true);
-		expect(axios.put).toHaveBeenCalledWith(
-			`/api/team/${teamId}/thought/${100}/discuss`,
-			{
-				discussed: true,
-			},
-			mockConfig
-		);
+	describe('delete', () => {
+		it('should delete a thought', () => {
+			ThoughtService.delete(teamId, thoughtId);
+			expect(axios.delete).toHaveBeenCalledWith(
+				`/api/team/${teamId}/thought/${thoughtId}`,
+				mockConfig
+			);
+		});
 	});
 
-	it('should update message on thought', async () => {
-		await ThoughtService.updateMessage(teamId, 100, 'new message');
-		expect(axios.put).toHaveBeenCalledWith(
-			`/api/team/${teamId}/thought/${100}/message`,
-			{
-				message: 'new message',
-			},
-			mockConfig
-		);
+	describe('upvoteThought', () => {
+		it('should update the upvotes for a thought', () => {
+			ThoughtService.upvoteThought(teamId, thoughtId);
+			expect(axios.put).toHaveBeenCalledWith(
+				`/api/team/${teamId}/thought/${thoughtId}/heart`,
+				mockConfig
+			);
+		});
 	});
 
-	it('should update column on thought', async () => {
-		await ThoughtService.updateColumn(teamId, 100, 20);
-		expect(axios.put).toHaveBeenCalledWith(
-			`/api/team/${teamId}/thought/${100}/column-id`,
-			{
-				columnId: 20,
-			},
-			mockConfig
-		);
+	describe('updateDiscussionStatus', () => {
+		it('should update the discussion status of a thought', () => {
+			const discussedState = true;
+			ThoughtService.updateDiscussionStatus(teamId, thoughtId, discussedState);
+			expect(axios.put).toHaveBeenCalledWith(
+				`/api/team/${teamId}/thought/${thoughtId}/discuss`,
+				{ discussed: discussedState },
+				mockConfig
+			);
+		});
+	});
+
+	describe('updateMessage', () => {
+		it('should the message of a thought', () => {
+			const updatedThoughtMessage = 'I changed my mind..';
+			ThoughtService.updateMessage(teamId, thoughtId, updatedThoughtMessage);
+			expect(axios.put).toHaveBeenCalledWith(
+				`/api/team/${teamId}/thought/${thoughtId}/message`,
+				{ message: updatedThoughtMessage },
+				mockConfig
+			);
+		});
+	});
+
+	describe('updateColumn', () => {
+		it('should update column on thought', async () => {
+			await ThoughtService.updateColumn(teamId, 100, 20);
+			expect(axios.put).toHaveBeenCalledWith(
+				`/api/team/${teamId}/thought/${100}/column-id`,
+				{
+					columnId: 20,
+				},
+				mockConfig
+			);
+		});
 	});
 });
