787aa4ba5ac2fadabac1c39afad704cd10ac98b8
Ashley Clifton <randomashes@gmail.com>
2022-04-08 10:10:03 -0400

    Create a Textarea component

---
+++ ui/src/Common/Textarea/Textarea.spec.tsx
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2022 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Textarea from './Textarea';
+
+describe('Textarea', () => {
+	const mockOnChange = jest.fn();
+	const mockOnEnter = jest.fn();
+	const mockInitialValue = 'Sample Text';
+
+	beforeEach(() => {
+		Object.defineProperty(HTMLElement.prototype, 'scrollHeight', {
+			configurable: true,
+			value: 100,
+		});
+
+		render(
+			<Textarea
+				initialValue={mockInitialValue}
+				onEnter={mockOnEnter}
+				onChange={mockOnChange}
+			/>
+		);
+	});
+
+	it('should populate textarea with initial value', () => {
+		expect(getTextarea()).toHaveValue(mockInitialValue);
+	});
+
+	it('should auto focus and auto select textarea', () => {
+		expect(getTextarea()).toHaveFocus();
+	});
+
+	it('should set the size of the textarea based on textarea scrollHeight', () => {
+		expect(getTextarea()).toHaveAttribute('style', 'height: 100px;');
+	});
+
+	it('should show character count', () => {
+		screen.getByText('244');
+		userEvent.type(getTextarea(), '1234565789123456789123456789');
+		screen.getByText('227');
+	});
+
+	it('should auto select value & trigger onChange when text is typed into textarea', async () => {
+		userEvent.type(getTextarea(), 'Hey!');
+		expect(mockOnChange).toHaveBeenCalledWith('Hey!');
+	});
+
+	it('should trigger onEnter when user types and clicks enter', async () => {
+		userEvent.type(getTextarea(), 'Hello there!{Enter}');
+
+		await waitFor(() =>
+			expect(mockOnEnter).toHaveBeenCalledWith(
+				'Hello there!',
+				expect.anything()
+			)
+		);
+	});
+});
+
+function getTextarea() {
+	return screen.getByTestId('textareaField');
+}
---
+++ ui/src/Common/Textarea/Textarea.tsx
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2022 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { KeyboardEvent, useEffect, useRef, useState } from 'react';
+import classnames from 'classnames';
+
+import { onKeys } from '../../Utils/EventUtils';
+import FloatingCharacterCountdown from '../FloatingCharacterCountdown/FloatingCharacterCountdown';
+
+import './Textarea.scss';
+
+const MAX_LENGTH = 255;
+
+interface Props {
+	initialValue?: string;
+	onEnter?: (value: string, event: KeyboardEvent<HTMLTextAreaElement>) => void;
+	onChange: (value: string) => void;
+	className?: string;
+}
+
+function Textarea(props: Props) {
+	const {
+		initialValue,
+		onEnter = () => undefined,
+		onChange,
+		className,
+	} = props;
+
+	const [editValue, setEditValue] = useState(initialValue || '');
+
+	const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+	useEffect(() => {
+		resizeTextArea();
+	}, [editValue, initialValue]);
+
+	useEffect(() => {
+		setEditValue(initialValue || '');
+		textAreaRef.current?.focus();
+		textAreaRef.current?.select();
+	}, [initialValue]);
+
+	function resizeTextArea() {
+		const textArea = textAreaRef.current;
+		if (textArea) {
+			textArea.style.height = textArea.scrollHeight + 'px';
+		}
+	}
+
+	return (
+		<div className={classnames('text-area', className)}>
+			<textarea
+				aria-label="Text Area"
+				data-testid="textareaField"
+				className="text-area-field"
+				ref={textAreaRef}
+				value={editValue}
+				onChange={(event) => {
+					setEditValue(event.target.value);
+					onChange(event.target.value);
+				}}
+				onKeyDown={onKeys('Enter', (event) => onEnter(editValue, event))}
+				maxLength={MAX_LENGTH}
+			/>
+			<FloatingCharacterCountdown
+				characterCount={editValue.length}
+				charsAreRunningOutThreshold={50}
+				maxCharacterCount={MAX_LENGTH}
+			/>
+		</div>
+	);
+}
+
+export default Textarea;
