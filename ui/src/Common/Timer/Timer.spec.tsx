6ffe5516b5f2af1b3798c4e6bd206de70956e49b
Nick Reuter <thenewimagineer@gmail.com>
2022-04-11 16:42:43 -0400

    #349 Begin rewriting timer in react

---
+++ ui/src/Common/Timer/Timer.spec.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022. Ford Motor Company
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { act, fireEvent, render, screen } from '@testing-library/react';
+
+import Timer from './Timer';
+
+describe('Timer', () => {
+	beforeEach(function () {
+		jest.useFakeTimers();
+	});
+
+	afterEach(function () {
+		jest.useRealTimers();
+	});
+
+	it('should display 5 minutes as the default', () => {
+		render(<Timer />);
+		screen.getByText('05:00');
+	});
+
+	it('should not change time if play not pressed', () => {
+		render(<Timer />);
+		advanceTimersSafely(1010);
+		screen.getByText('05:00');
+	});
+
+	it('should count time down play button pressed', () => {
+		render(<Timer />);
+		fireEvent.click(screen.getByAltText('Start timer'));
+		advanceTimersSafely(3010);
+		screen.getByText('04:57');
+	});
+});
+
+function advanceTimersSafely(duration: number) {
+	act(() => {
+		jest.advanceTimersByTime(duration);
+	});
+}
---
+++ ui/src/Common/Timer/Timer.tsx
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2022. Ford Motor Company
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { useCallback, useEffect, useState } from 'react';
+import moment from 'moment';
+
+import playButton from '../../Assets/play-icon.svg';
+
+enum TimerOption {
+	// THIRTY_SECONDS = 30,
+	// ONE_MINUTE = 60,
+	FIVE_MINUTES = 300,
+	// TEN_MINUTES = 600,
+}
+
+function Timer(): JSX.Element {
+	const [timerRunning, setTimerRunning] = useState<boolean>(false);
+	const [secondsLeft, setSecondsLeft] = useState<number>(
+		TimerOption.FIVE_MINUTES
+	);
+
+	const decrementTime = useCallback(() => {
+		setSecondsLeft((currentSecondsLeft) => currentSecondsLeft - 1);
+	}, []);
+
+	useEffect(() => {
+		if (timerRunning) {
+			const id = setInterval(decrementTime, 1000);
+			return () => clearInterval(id);
+		}
+	}, [timerRunning, decrementTime]);
+
+	function handlePlayPressed() {
+		setTimerRunning(true);
+	}
+
+	return (
+		<div>
+			<div>{secondsLeft}</div>
+			<div>{moment(secondsLeft * 1000).format('mm:ss')}</div>
+			<button onClick={handlePlayPressed}>
+				<img src={playButton} alt="Start timer" />
+			</button>
+		</div>
+	);
+}
+
+export default Timer;
