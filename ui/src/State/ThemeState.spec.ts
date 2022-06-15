d49d1b6ee18c3fe4197b646c96764c80504bebe5
Ashley Clifton <randomashes@gmail.com>
2022-06-15 09:55:44 -0400

    Add tests for getThemeClassFromUserSettings function

--- ui/src/State/ThemeState.spec.ts
+++ ui/src/State/ThemeState.spec.ts
@@ -17,12 +17,49 @@
 
 import { snapshot_UNSTABLE } from 'recoil';
 
-import { ThemeState } from './ThemeState';
+import Theme from '../Types/Theme';
+
+import { getThemeClassFromUserSettings, ThemeState } from './ThemeState';
 
 describe('Theme State', () => {
-	it('should default theme to system theme', () => {
-		const initialSnapshot = snapshot_UNSTABLE();
-		const actual = initialSnapshot.getLoadable(ThemeState).valueOrThrow();
-		expect(actual).toEqual('system-theme');
+	describe('ThemeState', () => {
+		it('should default theme to system theme', () => {
+			const initialSnapshot = snapshot_UNSTABLE();
+			const actual = initialSnapshot.getLoadable(ThemeState).valueOrThrow();
+			expect(actual).toEqual('system-theme');
+		});
+	});
+
+	describe('getThemeClassFromUserSettings', () => {
+		it('should return light theme when localstorage theme is set to "light-theme"', () => {
+			window.localStorage.setItem('theme', Theme.LIGHT);
+			expect(getThemeClassFromUserSettings()).toBe('light-theme');
+		});
+
+		it('should return dark theme when localstorage theme is set to "dark-theme"', () => {
+			window.localStorage.setItem('theme', Theme.DARK);
+			expect(getThemeClassFromUserSettings()).toBe('dark-theme');
+		});
+
+		it('should return system preferences when no theme is set in localstorage', () => {
+			window.localStorage.removeItem('theme');
+			(window.matchMedia as jest.Mock).mockReturnValue({ matches: true });
+			expect(getThemeClassFromUserSettings()).toBe('dark-theme');
+
+			(window.matchMedia as jest.Mock).mockReturnValue({ matches: false });
+			expect(getThemeClassFromUserSettings()).toBe('light-theme');
+		});
+
+		it('should return light theme when localstorage is set to "system-theme" and system preferences are light mode', () => {
+			window.localStorage.setItem('theme', Theme.SYSTEM);
+			(window.matchMedia as jest.Mock).mockReturnValue({ matches: false });
+			expect(getThemeClassFromUserSettings()).toBe('light-theme');
+		});
+
+		it('should return dark theme when localstorage is set to "system-theme" and system preferences are dark mode', () => {
+			window.localStorage.setItem('theme', Theme.SYSTEM);
+			(window.matchMedia as jest.Mock).mockReturnValue({ matches: true });
+			expect(getThemeClassFromUserSettings()).toBe('dark-theme');
+		});
 	});
 });
