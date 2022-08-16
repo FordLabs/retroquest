b51caad3d626788a663024c3a20935645f9e9943
Connor Shaughnessy <sapphon@gmail.com>
2022-08-16 16:36:06 -0400

    Add /api/config GET for configuration properties
    We will use this to store a url to a feedback survey

---
+++ api/src/test/java/com/ford/labs/retroquest/config/ConfigurationApiTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ford.labs.retroquest.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ConfigurationApiTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldReturnPropertyValueAsData() throws Exception {
+        var mvcResult = mockMvc.perform(get("/api/config"))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString())
+                .isEqualTo("{\"survey_link_href\":\"test-survey-link-href\"}");
+    }
+}
