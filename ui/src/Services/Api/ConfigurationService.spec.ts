1bac90b3415b25e21f8a101611760fb33e8a3787
Connor Shaughnessy <sapphon@gmail.com>
2022-08-17 11:38:36 -0400

    Test that survey link does not appear for blank survey href

--- ui/src/Services/Api/ConfigurationService.spec.ts
+++ ui/src/Services/Api/ConfigurationService.spec.ts
@@ -1,11 +1,14 @@
-import configurationService from "./ConfigurationService";
-import axios from "axios";
+import axios from 'axios';
+
+import configurationService from './ConfigurationService';
 
 describe('the configuration service', () => {
-	it('should perform a GET to /api/config', () => {
-		let value = {data:{propertyName:"propertyValue"}};
+	it('should perform a GET to /api/config', async () => {
+		let value = { data: { propertyName: 'propertyValue' } };
 		axios.get = jest.fn().mockResolvedValue(value);
-		configurationService.get().then(result => expect(result).toEqual(value.data));
+		await configurationService
+			.get()
+			.then((result) => expect(result).toEqual(value.data));
 		expect(axios.get).toHaveBeenCalledWith('/api/config');
 	});
-})
+});
