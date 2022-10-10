b92330c8b2e64f63b8dfe5c82d5e0f141d4e171e
Ashley Clifton <randomashes@gmail.com>
2022-10-10 15:54:46 -0400

    Add get team by email reset token
    * And copy endpoints from password reset token to email reset token (/lifetime-in-seconds and /{emailResetToken}/is-valid)

--- api/src/main/java/com/ford/labs/retroquest/email_reset_token/EmailResetTokenController.java
+++ api/src/main/java/com/ford/labs/retroquest/email_reset_token/EmailResetTokenController.java
@@ -14,39 +14,51 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ford.labs.retroquest.password_reset_token;
+package com.ford.labs.retroquest.email_reset_token;
 
+import com.ford.labs.retroquest.team.Team;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/password-reset-token")
+@RequestMapping(value = "/api/email-reset-token")
 @Tag(name = "Password Reset Token Controller", description = "The controller that manages password reset tokens")
-public class PasswordResetTokenController {
+public class EmailResetTokenController {
 
-    private final PasswordResetTokenRepository passwordResetTokenRepository;
+    private final EmailResetTokenRepository emailResetTokenRepository;
 
-    public PasswordResetTokenController(PasswordResetTokenRepository passwordResetRepository) {
-        this.passwordResetTokenRepository = passwordResetRepository;
+    public EmailResetTokenController(EmailResetTokenRepository passwordResetRepository) {
+        this.emailResetTokenRepository = passwordResetRepository;
     }
 
     @GetMapping("/lifetime-in-seconds")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
-    @Operation(description = "Get the number of seconds before a password reset token expires")
+    @Operation(description = "Get the number of seconds before an email reset token expires")
     public ResponseEntity<Integer> getResetTokenValiditySeconds(@Value("${retroquest.password.reset.token-lifetime-seconds:600}") int tokenSeconds){
         return ResponseEntity.ok(tokenSeconds);
     }
 
-    @GetMapping("/{passwordResetToken}/is-valid")
+    @GetMapping("/{emailResetToken}/is-valid")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
-    @Operation(description = "Check if the password reset token is valid")
-    public boolean checkResetTokenStatus(@PathVariable("passwordResetToken") String passwordResetToken) {
-        PasswordResetToken token = passwordResetTokenRepository.findByResetToken(passwordResetToken);
+    @Operation(description = "Check if the email reset token is valid")
+    public boolean checkResetTokenStatus(@PathVariable("emailResetToken") String emailResetToken) {
+        EmailResetToken token = emailResetTokenRepository.findByResetToken(emailResetToken);
         return token != null && !token.isExpired();
+    }
+
+    @GetMapping("/{emailResetToken}/team")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
+    @Operation(description = "Get Team by reset token")
+    public Team getTeamByResetToken(@PathVariable("emailResetToken") String emailResetToken) {
+        EmailResetToken token = emailResetTokenRepository.findByResetToken(emailResetToken);
+        return token.getTeam();
     }
 }
