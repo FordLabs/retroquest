89d910f9731e93974f46d038c3b6e0691d746fa5
Ashley Clifton <randomashes@gmail.com>
2022-11-22 16:43:17 -0500

    Change cypress viewport height and width

--- ui/cypress.config.ts
+++ ui/cypress.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
 	screenshotsFolder: 'cypress/artifacts/screenshots',
 	video: false,
 	chromeWebSecurity: false,
+	viewportWidth: 2000,
+	viewportHeight: 1484,
 	e2e: {
 		baseUrl: 'http://localhost:3000',
 		specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
