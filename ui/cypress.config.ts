bd4b5fb4a06eb2fafb604862bdd5f76a8a667d79
Ashley Clifton <randomashes@gmail.com>
2022-11-23 13:51:31 -0500

    Only save cypress screenshots on test failure (#514)

--- ui/cypress.config.ts
+++ ui/cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	downloadsFolder: 'cypress/downloads',
+	screenshotsFolder: 'cypress/artifacts/screenshots',
+	video: false,
 	chromeWebSecurity: false,
 	e2e: {
 		baseUrl: 'http://localhost:3000',
